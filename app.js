58ec70dd19a3f5bce6f809bbd26682f43f8a6e87
Bede-Ozy <bedeozy@gmail.com>
2025-04-23 13:34:30 +0100

    added interactivity for nav bar on mobile - added full mobile responsivity

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const body = document.querySelector("body"),
         blueyTheme = body.querySelector(".bluey-theme"),
-        purpleTheme = body.querySelector(".purple-theme");
+        purpleTheme = body.querySelector(".purple-theme"),
+        navToggle = body.querySelector(".nav-toggle"),
+        navBar = body.querySelector(".navbar"),
+        openCloseNav = body.querySelector(".open-close");
 
 blueyTheme.addEventListener("click", () => {
     body.classList.toggle("bluey");
+});
+
+
+navToggle.addEventListener("click", () => {
+    navBar.classList.toggle('show');
 });
